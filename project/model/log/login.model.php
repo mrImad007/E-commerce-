9bc841bd1c51ca6913a9e82d2fef8bf8d345fb4b
Imad Eddine Zaoui <imadeddinzaoui@gmail.com>
2022-12-20 09:29:44 +0100

    New everything

---
+++ project/model/log/login.model.php
@@ -0,0 +1,21 @@
+<?php
+    require_once('../database/database.model.php');
+
+    class login extends database{
+
+        public function check($name,$password){
+
+            $log = new database();
+            $pdo = $log->connection();
+
+            $query = "SELECT * FROM `admin` WHERE username = '$name' AND `password` = '$password' ";
+            $stet = $pdo->prepare($query);
+            $stet->execute();
+            $user = $stet->fetchAll(PDO::FETCH_ASSOC);
+
+            return $user;
+            }
+        }
+
+          
+?>
