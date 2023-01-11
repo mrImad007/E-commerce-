1cdec41415f2e862a7b510c183946354adeb2a71
mrImad007 <imadeddinzaoui@gmail.com>
2023-01-11 21:02:46 +0100

    controller commit

---
+++ project/controller/login.control.php
@@ -0,0 +1,13 @@
+<?php
+    require('../model/log/login.model.php');
+
+    class checklogin extends login {
+        public function doLogin(){
+                    
+        if(isset($_POST['username']) && isset($_POST['password'])){
+
+            $user = $_POST['username'];
+            $password = $_POST['password'];
+            
+            $conn = new login();
+            $conn->
